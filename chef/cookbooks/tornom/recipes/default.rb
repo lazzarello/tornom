2a7a2c2bec26cf3699e0d129d927aa07b27a98f4
Lee Azzarello <lee@rockingtiger.com>
2010-10-26 19:33:42 -0400

    Added cookbooks directory for ease of chef-solo deployment

---
+++ chef/cookbooks/tornom/recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: tornom
+# Recipe:: default
+#
+
+directory "/mnt/downloads" do
+  action :create
+  mode 0777
+end
+
+package "transmission-daemon"
+package "nginx"
+
+service "transmission-daemon" do
+  supports :restart => true, :reload => true
+  action :enable
+end
+
+service "nginx" do
+  supports :restart => true, :reload => true
+  action :enable
+end
+
+template "/etc/transmission-daemon/settings.json" do
+  source "settings.json.erb"
+  owner
+  group
+  mode
+  notifies :reload, "service[transmission-daemon]"
+end
+
+template "/etc/nginx/sites-available/default" do
+  source "nginx-default.erb"
+  owner
+  group
+  mode
+  notifies :restart, "service[nginx]"
+end
