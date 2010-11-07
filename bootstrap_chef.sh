b2597a32689b4a4183aa4effa5ef06c437cd81de
Lee Azzarello <lee@rockingtiger.com>
2010-11-07 18:31:37 -0500

    updated bootstrap script to create chef solo directory

--- bootstrap_chef.sh
+++ bootstrap_chef.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get --assume-yes upgrade
-apt-get install ruby1.8 rubygems irb libopenssl-ruby build-essential
+apt-get --assume-yes install ruby1.8 rubygems irb libopenssl-ruby build-essential ruby1.8-dev
 gem install chef --no-rdoc --no-ri
+mkdir /tmp/chef-solo
+chmod 777 /tmp/chef-solo
