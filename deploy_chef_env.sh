a6c7019cd1279c3d202a087a64b253d39c015c02
Lee Azzarello <lee@rockingtiger.com>
2010-11-07 19:56:07 -0500

    upload bootstrap_chef script, just to be safe

--- deploy_chef_env.sh
+++ deploy_chef_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+scp bootstrap_chef.sh ubuntu@$1:~/
 scp -r chef/cookbooks ubuntu@$1:/tmp/chef-solo/
 scp chef/config/* ubuntu@$1:/tmp/chef-solo/
 cat profile | ssh ubuntu@$1 "cat - >> ~/.profile"
