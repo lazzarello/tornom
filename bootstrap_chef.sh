59a43137c2979e9a13a3de1ff2bc45dc295b8097
Lee Azzarello <lee@rockingtiger.com>
2010-12-10 01:09:08 -0500

    don't run apt-get upgrade because it could hold up the init time

--- bootstrap_chef.sh
+++ bootstrap_chef.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get --assume-yes upgrade
 apt-get --assume-yes install ruby1.8 rubygems irb libopenssl-ruby build-essential ruby1.8-dev
 gem install chef --no-rdoc --no-ri
 mkdir /tmp/chef-solo
