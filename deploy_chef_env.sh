f70e14309d34cda2c4eedc0476a9015198e2464a
Lee Azzarello <lee@rockingtiger.com>
2010-12-10 00:51:59 -0500

    ssh to box after bootstrapping is done

--- deploy_chef_env.sh
+++ deploy_chef_env.sh
@@ -3,3 +3,4 @@ scp bootstrap_chef.sh ubuntu@$1:~/
 scp -r chef/cookbooks ubuntu@$1:/tmp/chef-solo/
 scp chef/config/* ubuntu@$1:/tmp/chef-solo/
 cat profile | ssh ubuntu@$1 "cat - >> ~/.profile"
+ssh ubuntu@$1
