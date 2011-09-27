c003166ff0bc1d1309fa452540fa141cb9ed6fa5
Lee Azzarello <lee@rockingtiger.com>
2011-09-27 18:21:03 -0400

    merged the chef deploy into the bootstrap script

--- deploy_chef_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scp bootstrap_chef.sh ubuntu@$1:~/
-scp -r chef/cookbooks ubuntu@$1:/tmp/chef-solo/
-scp chef/config/* ubuntu@$1:/tmp/chef-solo/
-cat profile | ssh ubuntu@$1 "cat - >> ~/.profile"
-ssh ubuntu@$1
