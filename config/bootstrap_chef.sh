b7a41027093642ad3fde47ec858f201f7a98db8c
Lee Azzarello <lee@rockingtiger.com>
2011-10-03 18:38:34 -0400

    exec options with defaults and wait_for progress riders

--- config/bootstrap_chef.sh
+++ config/bootstrap_chef.sh
@@ -15,6 +15,6 @@ wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
 chmod 755 ec2-metadata
 echo "tornom" > /etc/hostname
 hostname -F /etc/hostname
-echo "127.0.0.1 tornom localhost" > /etc/hosts
+echo "127.0.0.1 localhost.localdomain tornom localhost" > /etc/hosts
 /var/lib/gems/1.8/bin/chef-solo -c /tmp/chef-solo/solo.rb -j /tmp/chef-solo/config.json
 echo `curl -o - http://169.254.169.254/latest/meta-data/public-hostname` | mail -s tornom mail@example.com
