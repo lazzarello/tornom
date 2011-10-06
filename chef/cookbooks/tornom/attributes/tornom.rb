dec5f7451d562c59233fcb48a4f8201f80834998
Lee Azzarello <lee@rockingtiger.com>
2011-10-06 17:54:09 -0400

    nginx configuration changes for web app locations

--- chef/cookbooks/tornom/attributes/tornom.rb
+++ chef/cookbooks/tornom/attributes/tornom.rb
@@ -1,2 +1,4 @@
 set[:nginx][:docroot] = "/mnt/downloads"
+default[:nginx][:metrics] = "/var/cache/munin/www/"
 default[:tornom][:recipient_email] = "foo@bar.com"
+default[:nginx][:access_log] = "/var/log/nginx/localhost.access.log"
